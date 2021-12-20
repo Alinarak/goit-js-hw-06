8efba7dc02e8edc7588cb708303b01785d7bb5fa
Alinarak <alina.rak07@gmail.com>
2021-12-20 22:26:25 +0200

    change task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,18 @@
 const ref = {
   inputRef: document.querySelector("#validation-input"),
 };
+const toggleClasses = (addClass, removeClass) => {
+  ref.inputRef.classList.add(addClass);
+  ref.inputRef.classList.remove(removeClass);
+};
 
 ref.inputRef.addEventListener("blur", (event) => {
   if (
     ref.inputRef.value.length ===
     parseInt(ref.inputRef.getAttribute("data-length"))
   ) {
-    ref.inputRef.classList.add("valid");
-    ref.inputRef.classList.remove("invalid");
+    toggleClasses("valid", "invalid");
   } else {
-    ref.inputRef.classList.add("invalid");
-    ref.inputRef.classList.remove("valid");
+    toggleClasses("invalid", "valid");
   }
 });
